e044c90d3bee356fc6bdbdf7113d842b51329769
gena <gennadiy.donchyts@gmail.com>
2017-06-26 17:05:48 +0200

    change directory to local, add curl for windows

--- src/cmd/get_catchment.cmd
+++ src/cmd/get_catchment.cmd
@@ -2,5 +2,5 @@
 
 echo Retrieving catchment boundary ...
 
-rem curl -H "Content-Type: application/json" -X POST -d @query.json http://hydro-earth.appspot.com/get_catchment -o results.json
-.\bin\curl -H "Content-Type: application/json" -X POST -d @query.json http://localhost:8080/get_catchment -o results.json
+.\bin\curl -H "Content-Type: application/json" -X POST -d @query.json http://hydro-earth.appspot.com/get_catchment -o results.json
+rem .\bin\curl -H "Content-Type: application/json" -X POST -d @query.json http://localhost:8080/get_catchment -o results.json
