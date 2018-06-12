cec7b5d6ca0c985fb9675fff45e7e530adb2e906
Fedor Baart <fedor.baart@deltares.nl>
2018-06-12 15:45:37 +0200

    rewrite get_image_urls to get_bathymetry

--- src/app/main.py
+++ src/app/main.py
@@ -44,16 +44,20 @@ rivers = ee.FeatureCollection('users/gena/HydroEngine/riv_15s_lev05')
 # HydroLAKES
 lakes = ee.FeatureCollection('users/gena/HydroLAKES_polys_v10')
 
+
+# available datasets
+bathymetry = {
+    'jetski': ee.ImageCollection('users/gena/eo-bathymetry/sandengine_jetski'),
+    'vaklodingen': ee.ImageCollection('users/gena/vaklodingen'),
+    'kustlidar': ee.ImageCollection('users/gena/eo-bathymetry/rws_lidar')
+}
+
 # graph index
 index = ee.FeatureCollection('users/gena/HydroEngine/hybas_lev06_v1c_index')
 
 monthly_water = ee.ImageCollection('JRC/GSW1_0/MonthlyHistory')
 
 # bathymetry
-bathymetry_vaklodingen = ee.ImageCollection('users/gena/vaklodingen')
-bathymetry_jetski = ee.ImageCollection(
-    'users/gena/eo-bathymetry/sandengine_jetski')
-bathymetry_lidar = ee.ImageCollection('users/gena/eo-bathymetry/rws_lidar')
 
 
 def get_upstream_catchments(level):
@@ -103,7 +107,6 @@ def reduceImageProfile(image, line, reducer, scale):
 
     return image.reduceRegions(lines, reducer.setOutputs(band_names), scale)
 
-
 @app.route('/get_image_urls', methods=['GET', 'POST'])
 @flask_cors.cross_origin()
 def api_get_image_urls():
@@ -122,10 +125,11 @@ def api_get_image_urls():
     # TODO: Make option for how the interval is chosen (now only forward)
     t_count = 10
 
+
     rasters = {
-        'bathymetry_jetski': bathymetry_jetski,
-        'bathymetry_vaklodingen': bathymetry_vaklodingen,
-        'bathymetry_lidar': bathymetry_lidar
+        'bathymetry_jetski': bathymetry['jetski'],
+        'bathymetry_vaklodingen': bathymetry['vaklodingen'],
+        'bathymetry_lidar': bathymetry['kustlidar']
     }
 
     colorbar_min = {
@@ -182,6 +186,89 @@ def api_get_image_urls():
 
     return resp
 
+@app.route('/get_bathymetry', methods=['GET', 'POST'])
+@flask_cors.cross_origin()
+def api_get_bathymetry():
+    """generate bathymetry image for a certain timespan (begin_date, end_date) and a dataset {jetski | vaklodingen | kustlidar}"""
+    r = request.get_json()
+
+    # get info from the request
+    dataset = r['dataset']
+    begin_date = ee.Date(r['begin_date'])
+    end_date = ee.Date(r['end_date'])
+
+    colorbar_min = {
+        'jetski': -12,
+        'vaklodingen': -12,
+        # TODO: specify units in the dataset
+        'kustlidar': -1200
+    }
+
+    colorbar_max = {
+        'jetski': 7,
+        'vaklodingen': 7,
+        'kustlidar': 700
+    }
+
+    # TODO move to something configurable
+    # I believe this was created by Shore
+    sandengine_pallete = '''#000033,#000037,#00003a,#00003e,#000042,#000045,#000049,#00004d,#000050,#000054,#000057,#00005b,#00005f,#000062,#000066,#010268,#03036a,#04056c,#05076e,#070971,#080a73,#0a0c75,#0b0e77,#0c1079,#0e117b,#0f137d,#10157f,#121781,#131884,#141a86,#161c88,#171e8a,#191f8c,#1a218e,#1b2390,#1d2492,#1e2695,#1f2897,#212a99,#222b9b,#242d9d,#252f9f,#2a35a2,#2e3ca6,#3342a9,#3848ac,#3c4faf,#4155b3,#465cb6,#4a62b9,#4f68bc,#546fc0,#5875c3,#5d7bc6,#6282ca,#6688cd,#6b8fd0,#7095d3,#749bd7,#79a2da,#7ea8dd,#82aee0,#87b5e4,#8cbbe7,#90c2ea,#95c8ed,#9acef1,#9ed5f4,#a3dbf7,#a8e1fa,#9edef7,#94daf4,#8ad6f0,#80d2ed,#84cacb,#87c2a9,#8bba87,#8eb166,#92a944,#95a122,#999900,#a4a50b,#afb116,#babd21,#c5c92c,#d0d537,#dce142,#e7ec4d,#f2f857,#f3f658,#f3f359,#f4f15a,#f5ee5b,#f6eb5c,#f6e95d,#f7e65d,#f8e35e,#f9e15f,#fade60,#fadc61,#fbd962,#fcd663,#fdd463,#fdd164,#fecf65,#ffcc66,#fdc861,#fcc55d,#fbc158,#f9be53,#f7ba4f,#f6b64a,#f5b346,#f3af41,#f1ac3c,#f0a838,#efa433,#eda12e,#eb9d2a,#ea9a25,#e99620,#e7931c,#e58f17,#e48b13,#e3880e,#e18409,#df8105,#de7d00'''
+
+    def sorted_composite(images):
+        '''Create a sorted composite of the images between begin and end time, based on the system:time_start attribute'''
+        # sort by system time
+        sorted_images = images.sort('system:time_start', False)
+        # take first non missing pixel
+        sorted_composite = sorted_images.reduce(
+            ee.Reducer.firstNonNull()
+        )
+        # store metadata
+        return sorted_composite
+
+    def generate_mean_image(images):
+        reducer = ee.Reducer.mean()
+        mean_composite = images.reduce(reducer)
+        return mean_composite
+
+
+    def generate_image_info(image):
+        """generate url and tokens for image"""
+        image = ee.Image(image)
+        m = image.getMapId({
+            'min': colorbar_min[dataset],
+            'max': colorbar_max[dataset],
+            'palette': sandengine_pallete
+        })
+
+        mapid = m.get('mapid')
+        token = m.get('token')
+
+        url = 'https://earthengine.googleapis.com/map/{id}/{{z}}/{{x}}/{{y}}?token={token}'.format({
+            "id": id,
+            "token": token
+        })
+        result = {
+            'mapid': mapid,
+            'token': token,
+            'url': url
+        }
+        return result
+
+    # filter by date
+    images = bathymetry[dataset].filterDate(begin_date, end_date)
+
+    # create composite
+    image = sorted_composite(images)
+
+    # add metadata
+    info = generate_image_info(image)
+
+    info['begin'] = r['begin_date']
+    info['end'] = r['end_date']
+
+    resp = Response(json.dumps(info), status=200, mimetype='application/json')
+    return resp
+
 
 @app.route('/get_raster_profile', methods=['GET', 'POST'])
 @flask_cors.cross_origin()
@@ -196,9 +283,9 @@ def api_get_raster_profile():
     end_date = r['end_date']
 
     rasters = {
-        'bathymetry_jetski': bathymetry_jetski,
-        'bathymetry_vaklodingen': bathymetry_vaklodingen,
-        'bathymetry_lidar': bathymetry_lidar
+        'bathymetry_jetski': bathymetry['jetski'],
+        'bathymetry_vaklodingen': bathymetry['vaklodingen'],
+        'bathymetry_lidar': bathymetry['kustlidar']
     }
 
     raster = rasters[dataset]
