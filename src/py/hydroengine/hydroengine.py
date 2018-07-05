ec75291ba7bc3a3362fc403e0520c6f0cc1224f6
Martijn Visser <mgvisser@gmail.com>
2018-07-05 15:04:34 +0200

    add try except around requests
    With this users can get an idea of what went wrong, showing the requests error followed by the server response, with a full traceback after:
    
    ```
    500 Server Error: INTERNAL SERVER ERROR for url: http://hydro-engine.appspot.com/get_raster
    {
      "error": {
        "message": "'dtm'",
        "type": "UnexpectedException"
      },
      "success": false
    }
    
    Traceback (most recent call last):
      File "c:\bin\Miniconda3\envs\wflow3\lib\runpy.py", line 193, in _run_module_as_main
        "__main__", mod_spec)
      File "c:\bin\Miniconda3\envs\wflow3\lib\runpy.py", line 85, in _run_code
        exec(code, run_globals)
      File "d:\repo\hydro-model-builder\hydro_model_builder\cli.py", line 72, in <module>
        main(sys.argv[1:])  # pragma: no cover
      File "c:\bin\Miniconda3\envs\wflow3\lib\site-packages\click\core.py", line 722, in __call__
        return self.main(*args, **kwargs)
      File "c:\bin\Miniconda3\envs\wflow3\lib\site-packages\click\core.py", line 697, in main
        rv = self.invoke(ctx)
      File "c:\bin\Miniconda3\envs\wflow3\lib\site-packages\click\core.py", line 1066, in invoke
        return _process_result(sub_ctx.command.invoke(sub_ctx))
      File "c:\bin\Miniconda3\envs\wflow3\lib\site-packages\click\core.py", line 895, in invoke
        return ctx.invoke(self.callback, **ctx.params)
      File "c:\bin\Miniconda3\envs\wflow3\lib\site-packages\click\core.py", line 535, in invoke
        return callback(*args, **kwargs)
      File "d:\repo\hydro-model-builder\hydro_model_builder\cli.py", line 34, in generate_model
        general_options(genopt)
      File "d:\repo\hydro-model-builder\hydro_model_builder\cli.py", line 57, in general_options
        get_hydro_data(region, ds)
      File "d:\repo\hydro-model-builder\hydro_model_builder\cli.py", line 48, in get_hydro_data
        ds["catchment_level"],
      File "d:\repo\hydro-engine\src\py\hydroengine\hydroengine.py", line 126, in download_raster
        raise e
      File "d:\repo\hydro-engine\src\py\hydroengine\hydroengine.py", line 121, in download_raster
        r.raise_for_status()
      File "c:\bin\Miniconda3\envs\wflow3\lib\site-packages\requests\models.py", line 935, in raise_for_status
        raise HTTPError(http_error_msg, response=self)
    requests.exceptions.HTTPError: 500 Server Error: INTERNAL SERVER ERROR for url: http://hydro-engine.appspot.com/get_raster
    ```

--- src/py/hydroengine/hydroengine.py
+++ src/py/hydroengine/hydroengine.py
@@ -11,20 +11,32 @@ import os.path
 SERVER_URL = 'http://hydro-engine.appspot.com'
 
 
+def _check_request(r):
+    """Check for RequestException and print a useful error message"""
+    try:
+        r.raise_for_status()
+    except requests.RequestException as e:
+        print(e)
+        print(r.text)
+        raise e
+
+
 def download_water_mask(region, path):
     data = {'type': 'get_water_mask', 'region': region, 'use_url': True}
     r = requests.post(SERVER_URL + '/get_water_mask', json=data)
+    _check_request(r)
 
     # download from url
     r = requests.get(json.loads(r.text)['url'], stream=True)
-    if r.status_code == 200:
-        with open(path, 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
+    _check_request(r)
+    with open(path, 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
 
 def get_water_mask(region):
     data = {'type': 'get_water_mask', 'region': region, 'use_url': False}
     r = requests.post(SERVER_URL + '/get_water_mask', json=data)
+    _check_request(r)
 
     return r.text
 
@@ -32,6 +44,7 @@ def download_catchments(region, path, region_filter, catchment_level):
     data = {'type': 'get_catchments', 'region': region, 'dissolve': True,
             'region_filter': region_filter, 'catchment_level': catchment_level}
     r = requests.post(SERVER_URL + '/get_catchments', json=data)
+    _check_request(r)
 
     with open(path, 'w') as file:
         file.write(r.text)
@@ -45,19 +58,22 @@ def download_rivers(region, path, filter_upstream_gt, region_filter, catchment_l
         data['filter_upstream_gt'] = filter_upstream_gt
 
     r = requests.post(SERVER_URL + '/get_rivers', json=data)
+    _check_request(r)
 
     # download from url
     r = requests.get(json.loads(r.text)['url'], stream=True)
-    if r.status_code == 200:
-        with open(path, 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
+    _check_request(r)
+
+    with open(path, 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
 
 def get_lake_time_series(lake_id, variable, scale=0):
     data = {'type': 'get_lake_time_series', 'lake_id': lake_id,
             'variable': variable, 'scale': scale}
 
     r = requests.post(SERVER_URL + '/get_lake_time_series', json=data)
+    _check_request(r)
 
     return json.loads(r.text)
 
@@ -75,21 +91,23 @@ def get_lakes(region, id_only=False):
     data = {'type': 'get_lakes', 'region': region, 'id_only': id_only}
 
     r = requests.post(SERVER_URL + '/get_lakes', json=data)
+    _check_request(r)
 
     if id_only:
         return json.loads(r.text)
     else:
         r = requests.get(json.loads(r.text)['url'], stream=True)
+        _check_request(r)
 
-        if r.status_code == 200:
-            r.raw.decode_content = True
-            return json.loads(r.raw.data.decode('utf-8'))
+        r.raw.decode_content = True
+        return json.loads(r.raw.data.decode('utf-8'))
 
 
 def get_lake_ids(region):
     data = {'type': 'get_lakes', 'region': region, 'id_only': True}
 
     r = requests.post(SERVER_URL + '/get_lakes', json=data)
+    _check_request(r)
 
     return json.loads(r.text)
 
@@ -98,6 +116,7 @@ def get_lake_by_id(lake_id):
     data = {'type': 'get_lake_by_id', 'lake_id': lake_id}
 
     r = requests.post(SERVER_URL + '/get_lake_by_id', json=data)
+    _check_request(r)
 
     return json.loads(r.text)
 
@@ -114,13 +133,15 @@ def download_lakes(region, path, id_only):
     data = {'type': 'get_lakes', 'region': region, 'id_only': id_only}
 
     r = requests.post(SERVER_URL + '/get_lakes', json=data)
+    _check_request(r)
 
     # download from url
     r = requests.get(json.loads(r.text)['url'], stream=True)
-    if r.status_code == 200:
-        with open(path, 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
+    _check_request(r)
+
+    with open(path, 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
 
 
 def download_raster(region, path, variable, cell_size, crs, region_filter,
@@ -132,37 +153,39 @@ def download_raster(region, path, variable, cell_size, crs, region_filter,
             'catchment_level': catchment_level}
 
     r = requests.post(SERVER_URL + '/get_raster', json=data)
+    _check_request(r)
 
     # download from url
     r = requests.get(json.loads(r.text)['url'], stream=True)
-    if r.status_code == 200:
-        # download zip into a temporary file
-        with tempfile.NamedTemporaryFile(delete=False) as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
+    _check_request(r)
 
-        temp_dir = tempfile.mkdtemp()
+    # download zip into a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
 
-        # unzip and rename both tfw and tif
-        with zipfile.ZipFile(f.name, 'r') as zf:
-            items = zf.namelist()
-            zf.extractall(temp_dir)
+    temp_dir = tempfile.mkdtemp()
 
-        # move extracted files to the target path
-        src_tfw = os.path.join(temp_dir, items[0])
-        dst_tfw = path_name + '.tfw'
-        if os.path.exists(dst_tfw):
-            os.remove(dst_tfw)
-        shutil.move(src_tfw, dst_tfw)
+    # unzip and rename both tfw and tif
+    with zipfile.ZipFile(f.name, 'r') as zf:
+        items = zf.namelist()
+        zf.extractall(temp_dir)
 
-        src_tif = os.path.join(temp_dir, items[1])
-        if os.path.exists(path):
-            os.remove(path)
-        shutil.move(src_tif, path)
+    # move extracted files to the target path
+    src_tfw = os.path.join(temp_dir, items[0])
+    dst_tfw = path_name + '.tfw'
+    if os.path.exists(dst_tfw):
+        os.remove(dst_tfw)
+    shutil.move(src_tfw, dst_tfw)
 
-        # clean-up
-        os.rmdir(temp_dir)
-        os.remove(f.name)
+    src_tif = os.path.join(temp_dir, items[1])
+    if os.path.exists(path):
+        os.remove(path)
+    shutil.move(src_tif, path)
+
+    # clean-up
+    os.rmdir(temp_dir)
+    os.remove(f.name)
 
 
 def download_raster_profile(region, path, variable, scale):
@@ -170,12 +193,12 @@ def download_raster_profile(region, path, variable, scale):
             'variable': variable, 'scale': scale}
 
     r = requests.post(SERVER_URL + '/get_raster_profile', json=data)
+    _check_request(r)
 
     # download from url
     profile = json.loads(r.text)
-    if r.status_code == 200:
-        with open(path, 'w') as f:
-            json.dump(profile, f)
+    with open(path, 'w') as f:
+        json.dump(profile, f)
 
 
 def main():
