79818294ac452fa670b4a88565cea2ee3d30b459
gena <gennadiy.donchyts@gmail.com>
2018-08-22 21:58:55 +0200

    switch to remote server

--- src/py/setup.py
+++ src/py/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="hydroengine",
     packages=['hydroengine'],
-    version="0.0.22",
+    version="0.0.23",
     install_requires=['requests>=2.12'],
     entry_points={
         'console_scripts': [
