fefa0404599940ab9464772841a4abbec6e4ced2
gena <gennadiy.donchyts@gmail.com>
2018-08-27 11:09:22 +0200

    add missing requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0', 'requests>=2.12']
 
 setup_requirements = ['pytest-runner', ]
 
